da2049396c82606218fb4a4ccb4fca600952e531
shiftj18 <shiftj18@gmail.com>
2024-10-14 12:28:50 +0800

    feat: merge babel-plugin-remove-unused-import, support require

--- src/index.js
+++ src/index.js
@@ -6,29 +6,90 @@
  */
 const match = require('./utils/matchRule')
 
+function isIgnore(opts, name) {
+  return opts && opts.ignore && match(opts.ignore, name)
+}
+
 module.exports = function(babel) {
   const t = babel.types
   return {
     visitor: {
       Program: {
         enter: (path, { opts } = {}) => {
-          for (const [name, binding] of Object.entries(path.scope.bindings)) {
-            if (binding.kind === 'module') {
-              if (!binding.referenced) {
-                const source = binding.path.parentPath.get('source')
+          const bindings = path.scope.bindings
+          const UnRefBindings = new Map()
+
+          for (const [name, binding] of Object.entries(bindings)) {
+            if (!binding.path.parentPath || binding.kind !== 'module') continue
+
+            const source = binding.path.parentPath.get('source')
+            const importName = source.node.value
+            if (!t.isStringLiteral(source) || isIgnore(opts, importName))
+              continue
+
+            const key =
+              importName +
+              '(' +
+              (source.node.loc && source.node.loc.start.line) +
+              ')'
+            if (!UnRefBindings.has(key)) {
+              UnRefBindings.set(key, binding)
+            }
+
+            if (binding.referenced) {
+              UnRefBindings.set(key, null)
+            } else {
+              const nodeType = binding.path.node.type
+              if (nodeType === 'ImportSpecifier') {
+                binding.path.remove()
+              } else if (nodeType === 'ImportDefaultSpecifier') {
+                binding.path.remove()
+              } else if (nodeType === 'ImportNamespaceSpecifier') {
+                binding.path.remove()
+              } else if (binding.path.parentPath) {
+                binding.path.parentPath.remove()
+              }
+            }
+          }
+
+          UnRefBindings.forEach(function(binding, key) {
+            if (binding && binding.path.parentPath) {
+              binding.path.parentPath.remove()
+            }
+          })
+
+          path.traverse({
+            CallExpression(callPath) {
+              // 检查是否是 require()
+              if (t.isIdentifier(callPath.node.callee, { name: 'require' })) {
+                const source = callPath.node.arguments[0]
                 if (
                   t.isStringLiteral(source) &&
-                  (!opts.ignore || !match(opts.ignore, source.node.value))
+                  !isIgnore(opts, source.value)
                 ) {
-                  if (binding.path.node.type === 'ImportSpecifier') {
-                    binding.path.remove()
-                  } else if (binding.path.parentPath) {
-                    binding.path.parentPath.remove()
+                  if (t.isVariableDeclarator(callPath.parent)) {
+                    if (t.isIdentifier(callPath.parent.id)) {
+                      // 情况 const a = require('a');
+                      const objName =
+                        callPath.parent.id && callPath.parent.id.name
+                      if (objName && !bindings[objName].referenced) {
+                        callPath.parentPath.parentPath.remove()
+                      }
+                    } else if (t.isObjectPattern(callPath.parent.id)) {
+                      // 情况 const { c1, c2 } = require('c');
+                      // 情况 const { d1: rename, d2 } = require('c');
+                      const keys = callPath.parent.id.properties.map(
+                        it => it.value.name
+                      )
+                      if (keys.every(key => !bindings[key].referenced)) {
+                        callPath.parentPath.parentPath.remove()
+                      }
+                    }
                   }
                 }
               }
             }
-          }
+          })
         }
       }
     }
