f56a8fd881e5360770890f9114f2328c41b48b8e
shiftj18 <shiftj18@gmail.com>
2024-10-26 00:28:19 +0800

    fix: compat var a = require(), ...;

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-/**
- * @author: Cuttle Cong、chuyik
- * @forker: shiftj18
- * @date: 2024/10/14
- * @description:
- */
 const match = require('./utils/matchRule')
 
 function isIgnore(opts, name) {
-  return opts && opts.ignore && match(opts.ignore, name)
+  return opts?.ignore && match(opts.ignore, name)
 }
 
 module.exports = function(babel) {
@@ -24,14 +18,9 @@ module.exports = function(babel) {
 
             const source = binding.path.parentPath.get('source')
             const importName = source.node.value
-            if (!t.isStringLiteral(source) || isIgnore(opts, importName))
-              continue
+            if (!t.isStringLiteral(source) || isIgnore(opts, importName)) continue
 
-            const key =
-              importName +
-              '(' +
-              (source.node.loc && source.node.loc.start.line) +
-              ')'
+            const key = `${importName}(${source.node.loc?.start.line})`
             if (!UnRefBindings.has(key)) {
               UnRefBindings.set(key, binding)
             }
@@ -52,7 +41,7 @@ module.exports = function(babel) {
             }
           }
 
-          UnRefBindings.forEach(function(binding, key) {
+          UnRefBindings.forEach(function(binding) {
             if (binding && binding.path.parentPath) {
               binding.path.parentPath.remove()
             }
@@ -60,29 +49,39 @@ module.exports = function(babel) {
 
           path.traverse({
             CallExpression(callPath) {
-              // 检查是否是 require()
               if (t.isIdentifier(callPath.node.callee, { name: 'require' })) {
                 const source = callPath.node.arguments[0]
-                if (
-                  t.isStringLiteral(source) &&
-                  !isIgnore(opts, source.value)
-                ) {
+                if (t.isStringLiteral(source) && !isIgnore(opts, source.value)) {
                   if (t.isVariableDeclarator(callPath.parent)) {
                     if (t.isIdentifier(callPath.parent.id)) {
-                      // 情况 const a = require('a');
-                      const objName =
-                        callPath.parent.id && callPath.parent.id.name
-                      if (objName && !bindings[objName].referenced) {
-                        callPath.parentPath.parentPath.remove()
+                      // case `const a = require('a');`
+                      const objName = callPath.parent.id?.name
+                      if (objName && !bindings[objName]?.referenced) {
+                        const parent = callPath.parentPath.parent
+                        if (t.isVariableDeclaration(parent)) {
+                          if (parent.declarations.length > 1) {
+                            // var x1 = require('x1'), ...;
+                            // -> var ...;
+                            callPath.parentPath.remove()
+                          } else {
+                            callPath.parentPath.parentPath.remove()
+                          }
+                        }
                       }
                     } else if (t.isObjectPattern(callPath.parent.id)) {
-                      // 情况 const { c1, c2 } = require('c');
-                      // 情况 const { d1: rename, d2 } = require('c');
-                      const keys = callPath.parent.id.properties.map(
-                        it => it.value.name
-                      )
-                      if (keys.every(key => !bindings[key].referenced)) {
-                        callPath.parentPath.parentPath.remove()
+                      // case `const { c1, c2 } = require('c');`
+                      // case `const { d1: rename, d2 } = require('c');`
+                      const keys = callPath.parent.id.properties.map(it => it.value.name)
+                      if (keys.every(key => !bindings[key]?.referenced)) {
+                        if (t.isVariableDeclaration(parent)) {
+                          if (parent.declarations.length > 1) {
+                            // var { x1, x11 } = require('x1'), ...;
+                            // -> var ...;
+                            callPath.parentPath.remove()
+                          } else {
+                            callPath.parentPath.parentPath.remove()
+                          }
+                        }
                       }
                     }
                   }
